9852329cbfdd8797168e635edf706e918b169915
ilkinnazarli@gmail.com <ilkinnazarli@gmail.com>
2021-08-23 10:40:14 +0400

    init_2

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/comps'),
+      dir: require('path').join(__dirname, './coverage/copms'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'comps'`, () => {
+  it(`should have as title 'copms'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('comps');
+    expect(app.title).toEqual('copms');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('comps app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('copms app is running!');
   });
 });
